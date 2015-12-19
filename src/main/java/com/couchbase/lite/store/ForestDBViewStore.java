d8765716523c5be8a6a56c151ca9623eba99874d
hideki <hideki@couchbase.com>
2015-12-18 17:41:01 -0800

    removed unnecessary package

--- src/main/java/com/couchbase/lite/store/ForestDBViewStore.java
+++ src/main/java/com/couchbase/lite/store/ForestDBViewStore.java
@@ -45,7 +45,6 @@ import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
