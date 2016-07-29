7ec5c2ee70a33c1623e700b009b2e9c70218159d
hideki <hideki@couchbase.com>
2016-07-29 12:41:50 -0700

    Defined _ALIGN_MEM_ACCESS to c++ compilation too.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,12 +1,8 @@
+# APP_ABI := armeabi mips armeabi-v7a x86 arm64-v8a x86_64 mips64
 APP_ABI := all
-#APP_ABI := armeabi-v7a
-#APP_ABI := arm64-v8a x86_64
-#APP_ABI := armeabi mips armeabi-v7a x86 arm64-v8a x86_64 mips64
-#APP_ABI := armeabi-v7a x86 arm64-v8a x86_64 mips64
-#APP_ABI := all
 APP_PLATFORM := android-19
 # it seems no backward compatibility. 
-#APP_PLATFORM := android-21
+# APP_PLATFORM := android-21
 NDK_TOOLCHAIN_VERSION := clang
 APP_STL := gnustl_static
-#APP_OPTIM := debug
+# APP_OPTIM := debug # default is `release`
